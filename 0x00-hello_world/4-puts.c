69bc66a30181cadc181d660b0f8c93d7ed20719a
Fadzaiwendymuzadzi <fadzaiwendym@gmail.com>
2023-03-04 16:35:08 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
